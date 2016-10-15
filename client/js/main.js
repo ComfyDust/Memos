89f0c115036ae21f122a48a8f46a1a1ece359499
Kevin Yarosh <paranormallink@gmail.com>
2016-10-15 18:54:30 -0400

    Moved js files from client/ to client/js; created memo_list and memo_view HTML templates; added Meteor account packages

--- client/js/main.js
+++ client/js/main.js
@@ -5,7 +5,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+import '../templates/main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
